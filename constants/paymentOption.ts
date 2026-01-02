a38209a8c158da352e0d991211fe2bcbc56f28f1
Sakthivelan2005 <sakthivelan.shankar@gmail.com>
2026-01-02 23:28:37 +0530

    Added color and payment options

--- constants/paymentOption.ts
+++ constants/paymentOption.ts
@@ -1,5 +1,6 @@
- export const options = [
-    { id: 'apple', label: 'Apple Pay', image: require('@/assets/images/payment/options/Apple Pay.svg') },
+import { Icons } from "@/config/icons";
+export const options = [
+    { id: 'apple', label: 'Apple Pay', image: Icons.ApplePay},
     { id: 'paypal', label: 'PayPal', image: require('@/assets/images/payment/options/Paypal.svg') },
     { id: 'cash', label: 'Cash', image: require('@/assets/images/payment/options/cash.svg') },
   ];
