16179de796655e086266244309a166eeef1056ae
Sakthivelan2005 <sakthivelan.shankar@gmail.com>
2026-01-09 11:32:46 +0530

    added Colors and updated Payement

---
+++ constants/Colors.ts
@@ -0,0 +1,20 @@
+export const Colors = {
+  light: {
+    text: '#1F2121',
+    textSecondary: '#666666',
+    background: '#FCFCF9',
+    surface: '#FFFFFF',
+    border: '#E0E0E0',
+    primary: '#D4A5A5',
+    primaryDark: '#C48888',
+  },
+  dark: {
+    text: '#FFFFFF',
+    textSecondary: '#CCCCCC',
+    background: '#fff',
+    surface: '#2A2A2A',
+    border: '#404040',
+    primary: '#E8B8B8',
+    primaryDark: '#D4A5A5',
+  },
+};
--- constants/paymentOption.ts
+++ constants/paymentOption.ts
@@ -1,6 +1,5 @@
-import { Icons } from "@/config/icons";
 export const options = [
-    { id: 'apple', label: 'Apple Pay', image: Icons.ApplePay},
+    { id: 'apple', label: 'Apple Pay', image: require("@/assets/images/payment/options/Apple Pay.svg")},
     { id: 'paypal', label: 'PayPal', image: require('@/assets/images/payment/options/Paypal.svg') },
     { id: 'cash', label: 'Cash', image: require('@/assets/images/payment/options/cash.svg') },
   ];
